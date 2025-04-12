ecd73519c54af56a015d3106d3a1d6823f781baf
fbn776 <fbn776@gmail.com>
2025-04-12 17:35:39 +0530

    feat: points plot

--- utils/draw_landmark.py
+++ utils/draw_landmark.py
@@ -11,56 +11,61 @@ FONT_SIZE = 1
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54)
 
+# Only the middle finger and joints are used for points plot
+selected_indices = [5, 13, 17, 0, 9, 10, 11, 12]
+custom_connections = [(9, 10), (10, 11), (11, 12), (5, 9), (9, 13), (13, 17), (0, 5), (0, 9), (0, 13), (0, 17)]
+
 def draw_landmarks_on_image(rgb_image, detection_result):
     hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
+    # handedness_list = detection_result.handedness
     annotated_image = np.copy(rgb_image)
 
-    # Loop through the detected hands to visualize.
     for idx in range(len(hand_landmarks_list)):
         hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
 
         # Draw the hand landmarks.
         hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
         hand_landmarks_proto.landmark.extend([
             landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
         ])
-        solutions.drawing_utils.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            solutions.hands.HAND_CONNECTIONS,
-            solutions.drawing_styles.get_default_hand_landmarks_style(),
-            solutions.drawing_styles.get_default_hand_connections_style())
+
+
+        # solutions.drawing_utils.draw_landmarks(
+        #     annotated_image,
+        #     hand_landmarks_proto,
+        #     solutions.hands.HAND_CONNECTIONS,
+        #     solutions.drawing_styles.get_default_hand_landmarks_style(),
+        #     solutions.drawing_styles.get_default_hand_connections_style(),
+        #     # is_drawing_landmarks=False
+        # )
 
         # Get the top left corner of the detected hand's bounding box.
         height, width, _ = annotated_image.shape
         x_coordinates = [landmark.x for landmark in hand_landmarks]
         y_coordinates = [landmark.y for landmark in hand_landmarks]
 
-        coord = [(int(landmark.x * width), int(landmark.y * height)) for landmark in hand_landmarks]
+        for i in selected_indices:
+            lm = hand_landmarks[i]
+            cx, cy = int(lm.x * width), int(lm.y * height)
+            cv2.circle(annotated_image, (cx, cy), 6, (255, 0, 0), -1)  # Blue dots
 
-        wrist = coord[0]
-        finger_base = coord[9] # A
-        finger_lower_mid = coord[10] # B
-        finger_upper_mid = coord[11] # C
-        finger_tip = coord[12] # D
+        # Draw custom connections
+        for connection in custom_connections:
+            start_idx, end_idx = connection
+            x0 = int(hand_landmarks[start_idx].x * width)
+            y0 = int(hand_landmarks[start_idx].y * height)
+            x1 = int(hand_landmarks[end_idx].x * width)
+            y1 = int(hand_landmarks[end_idx].y * height)
+            cv2.line(annotated_image, (x0, y0), (x1, y1), (0, 255, 255), 2)
 
-        m_ad = compute_slope(finger_base, finger_tip)
-        m_bc = compute_slope(finger_lower_mid, finger_upper_mid)
-
-        is_in_line = abs(m_ad - m_bc) < 0.2 if m_ad != float('inf') and m_bc != float('inf') else m_ad == m_bc
-
-        # print("Is in line:", is_in_line, "Mad =", m_ad, "Mbc =", m_bc, "diff =", abs(m_ad - m_bc))
-
-        # Bounding box;
-        x_min, x_max = min(x_coordinates), max(x_coordinates)
-        y_min, y_max = min(y_coordinates), max(y_coordinates)
-
-        cv2.rectangle(annotated_image,
-                      (int(x_min * width), int(y_min * height)),
-                      (int(x_max * width), int(y_max * height)),
-                      (0, 255, 0), 2)
+        # # Bounding box;
+        # x_min, x_max = min(x_coordinates), max(x_coordinates)
+        # y_min, y_max = min(y_coordinates), max(y_coordinates)
+        #
+        # cv2.rectangle(annotated_image,
+        #               (int(x_min * width), int(y_min * height)),
+        #               (int(x_max * width), int(y_max * height)),
+        #               (0, 255, 0), 2)
 
         text_x = int(min(x_coordinates) * width)
         text_y = int(min(y_coordinates) * height) - MARGIN
@@ -75,11 +80,5 @@ def draw_landmarks_on_image(rgb_image, detection_result):
                         (text_x, text_y - 30), cv2.FONT_HERSHEY_DUPLEX,
                         FONT_SIZE, (0, 0, 255), FONT_THICKNESS, cv2.LINE_AA)
             return annotated_image, True
-
-
-        # Draw handedness (left or right hand) on the image.
-        cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
 
     return annotated_image, False
